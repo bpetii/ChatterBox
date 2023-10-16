1bef36f45da16e6feb44dd8d8129f3e6a5d049fb
Peter Biro <peter.biro@oliasoft.com>
2023-10-16 22:50:22 +0200

    added dashboard

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,14 +1,18 @@
-
+import React from 'react'
 import Siderbar from './Siderbar'
+import OpenConversation from './OpenConversation'
+import { useConversations } from '../context/ConversationsProvider'
 
 interface Props {
     id: string
 }
 
 const Dashboard = ({id}: Props) => {
+  const {selectedConversation} = useConversations();
   return (
     <div className='d-flex' style={{height: '100vh'}}>
         <Siderbar id={id}/>
+        {selectedConversation && <OpenConversation />}
     </div>
   )
 }
