6db6fe86edfd783e324e767b1dce769f20dd2a83
Peter Biro <peter.biro@oliasoft.com>
2023-10-17 08:16:54 +0200

    display conversation messages

--- client/src/components/OpenConversation.tsx
+++ client/src/components/OpenConversation.tsx
@@ -16,8 +16,19 @@ const OpenConversation = () => {
   return (
     <div className="d-flex flex-column flex-grow-1">
       <div className="flex-grow-1 overflow-auto">
-        <div>
+        <div className="d-flex flex-column alig-items-start justify-content-end px-3">
+          {selectedConversation.messages.map((message, index) => {
+            return (
+              <div
+                key={index}
+                className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end': '' }`}  
+              >
+                <p className={`rounded px-2 py-1 ${message.fromMe? 'bg-primary text-white' : 'border'} `}>{message.message}</p>
+                <div className={`text-muted small ${message.fromMe? 'text-right': ''}`}>{message.fromMe? 'You' : message.senderName}</div>
 
+              </div>
+            ) 
+          })}
         </div>
       </div>
       <Form className="m-2" onSubmit={handleSubmit}>
